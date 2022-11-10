0d9fd26d1c9eae5f53c26ed91a82685bd7ac9c7f
Muhammad Usman Asghar <42khan0@gmail.com>
2022-11-10 21:53:00 +0500

    Update Bts

---
+++ BTS.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+using namepace std;
+
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node *parent;
+    Node(int data)
+    {
+        this->data = data;
+        left = NULL;
+        right = NULL;
+        parent = NULL;
+    }
+};
+
+class BST
+{   
+    private:
+    Node *root;
+    public:
+        BST()
+        {
+            root = NULL;
+        }
+        bool isEmpty()
+        {
+            return root == NULL;
+        }
+        Node *getRoot()
+        {
+            return root;
+        }
+        Node *Insert(int x)
+        {
+            Node *temp = new Node(x);
+            if(isEmpty())
+            {
+                root = temp;
+            }
+            else
+            {
+                Node *record = root;
+                Node *parent = NULL;
+                while(record != NULL)
+                {
+                    parent = record;
+                    if(x < record->data)
+                    {
+                        record = record->left;
+                    }
+                    else
+                    {
+                        record = record->right;
+                    }
+                }
+                if(x < parent->data)
+                {
+                    parent->left = temp;
+                    temp->parent = parent;
+                }
+                else
+                {
+                    parent->right = temp;
+                    temp->parent = parent;
+                }
+            }
+            return temp;
+        }
+};
+int main()
+{
+
+}
