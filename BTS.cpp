29e5e198e2ca1bce59c0077801cd1f20019a4532
Muhammad Usman Asghar <42khan0@gmail.com>
2022-11-10 22:18:42 +0500

    delete node

--- BTS.cpp
+++ BTS.cpp
@@ -26,14 +26,20 @@ class BST
         {
             root = NULL;
         }
+
+        // EMPTY NODE
         bool isEmpty()
         {
             return root == NULL;
         }
+
+        // GET ROOT
         Node *getRoot()
         {
             return root;
         }
+
+        // INSERT NODE
         Node *Insert(int x)
         {
             Node *temp = new Node(x);
@@ -71,7 +77,7 @@ class BST
             return temp;
         }
 
-        // FIND 
+        // FIND NODE
         Node *find(int x)
         {
             Node *record = root;
@@ -91,6 +97,82 @@ class BST
                 }
             }
             return NULL;
+        }
+
+        Node *deleteNode(int x)
+        {
+            Node *temp = find(x);
+            if(temp == NULL)
+            {
+                return NULL;
+            }
+            else
+            {
+                if(temp->left == NULL && temp->right == NULL)
+                {
+                    if(temp->parent->left == temp)
+                    {
+                        temp->parent->left = NULL;
+                    }
+                    else
+                    {
+                        temp->parent->right = NULL;
+                    }
+                    delete temp;
+                }
+                else if(temp->left == NULL || temp->right == NULL)
+                {
+                    if(temp->left == NULL)
+                    {
+                        if(temp->parent->left == temp)
+                        {
+                            temp->parent->left = temp->right;
+                        }
+                        else
+                        {
+                            temp->parent->right = temp->right;
+                        }
+                        temp->right->parent = temp->parent;
+                        delete temp;
+                    }
+                    else
+                    {
+                        if(temp->parent->left == temp)
+                        {
+                            temp->parent->left = temp->left;
+                        }
+                        else
+                        {
+                            temp->parent->right = temp->left;
+                        }
+                        temp->left->parent = temp->parent;
+                        delete temp;
+                    }
+                }
+                else
+                {
+                    Node *record = temp->right;
+                    while(record->left != NULL)
+                    {
+                        record = record->left;
+                    }
+                    temp->data = record->data;
+                    if(record->parent->left == record)
+                    {
+                        record->parent->left = record->right;
+                    }
+                    else
+                    {
+                        record->parent->right = record->right;
+                    }
+                    if(record->right != NULL)
+                    {
+                        record->right->parent = record->parent;
+                    }
+                    delete record;
+                }
+            }
+            return temp;
         }
 };
 int main()
